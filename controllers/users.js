2ae81688aa7aade7a41eab0fa1781abf1440f08c
DarkMalk <sebastian.cea40@gmail.com>
2022-08-24 16:31:26 -0400

    Commented rute GET / ,rename import of helper/errors, rename variable of .then()

--- controllers/users.js
+++ controllers/users.js
@@ -1,28 +1,27 @@
 const { Router } = require('express')
 const bcrypt = require('bcrypt')
 const User = require('../models/User.js')
-const { Errors } = require('../helper/helpers.js')
+const Errors = require('../helper/Errors.js')
 const usersRouter = Router()
 
-usersRouter.get('/', async (req, res) => {
-  const users = await User.find({}).populate('notes', {
-    content: 1,
-    title: 1,
-    _id: 1,
-    date: 1
-  })
-  res.json(users)
-})
+// usersRouter.get('/', async (req, res) => {
+//   const users = await User.find({}).populate('notes', {
+//     content: 1,
+//     title: 1,
+//     _id: 1,
+//     date: 1
+//   })
+//   res.json(users)
+// })
 
 usersRouter.post('/', async (req, res, next) => {
   const { username, email, password } = req.body
   if (!username || !email || !password) return next(Errors.RequiredEmailUsernameAndPassword)
-  // return 
   const passwordHash = await bcrypt.hash(password, 10)
   const user = new User({ email, username, passwordHash })
   user
     .save()
-    .then(note => res.status(201).json(note))
+    .then(user => res.status(201).json(user))
     .catch(err => next(err))
 })
 
